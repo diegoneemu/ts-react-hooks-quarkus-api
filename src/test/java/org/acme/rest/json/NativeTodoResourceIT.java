9d526f736c9eb26cf183045e0a5c4f05449722a8
Diego Augusto <professor.diegosilva@gmail.com>
2019-08-30 10:11:49 -0300

    first commit

---
+++ src/test/java/org/acme/rest/json/NativeTodoResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.rest.json;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeTodoResourceIT extends TodoResourceTest {
+
+    // Execute the same tests but in native mode.
+}
